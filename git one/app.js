6cf143341c3382221eac8a805de9b2ad3a6a873a
Dayan Qazi <dayanqazi622@gmail.com>
2024-02-24 03:49:17 +0500

    Work Done

---
+++ git one/app.js
@@ -0,0 +1,72 @@
+// Q1,
+//    solve
+
+// let num = +prompt("Enter your num")
+
+
+// if(num%6 === 0){
+//     document.write("your num is even")
+// }
+// else{
+//     document.write("your num is odd")
+// }
+
+// ========================================================================================
+
+// Q2,
+//    solve
+
+// let score = +prompt("enter your percentage")
+// let grade;
+
+// if(score >=90 && score <=100){
+//     console.log("A+")
+// }else if(score >=70 && score <=89){
+//     console.log("A")
+// }else if(score >=0 && score <=49){
+//     console.log("Fail")
+// }
+
+
+// ===============================================================================================
+
+
+// Q3,
+//    solve
+
+
+// var name = prompt("name");
+// var English = +prompt("English")
+// var Socialstudies = +prompt("Socialstudies")
+// var Islamiat = +prompt("Islamiat")
+// var Urdu = +prompt("Urdu")
+// var Math = +prompt("Math")
+// var total = English + Socialstudies + Islamiat + Urdu + Math;
+// var percentage = (total / 500) * 100;
+
+
+// if(percentage >=90 && percentage <=100){
+//     console.log(name + "your grade is A+ " + percentage+"%");
+// }else if(percentage >=70 && percentage <=89){
+//     console.log(name + "your grade is A " + percentage+"%");
+// }else if(percentage >=40 && percentage <=69){
+//     console.log(name + "your grade is C " + percentage+"%");
+// }else if(percentage >=0 && percentage <=39){
+//     console.log(name + "your grade is E " + percentage+"%");
+// }else{
+//     console.log("Invalid Number")
+// }
+
+// ===============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
